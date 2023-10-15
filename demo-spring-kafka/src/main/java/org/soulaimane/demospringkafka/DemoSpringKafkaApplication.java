0c80899056d2ff0a3297ffb263390e6e3e015a25
SOULAIMANE BAHI <bahisolayman096@gmail.com>
2023-10-15 18:33:58 +0100

    first commit

---
+++ demo-spring-kafka/src/main/java/org/soulaimane/demospringkafka/DemoSpringKafkaApplication.java
@@ -0,0 +1,13 @@
+package org.soulaimane.demospringkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSpringKafkaApplication.class, args);
+    }
+
+}
---
+++ demo-spring-kafka/src/test/java/org/soulaimane/demospringkafka/DemoSpringKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.soulaimane.demospringkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
